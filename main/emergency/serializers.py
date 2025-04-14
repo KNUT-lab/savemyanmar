7283e4b3dcf227a3bca255d8184dddce26fca59e
ZTSmit <zedtosu@gmail.com>
2025-04-14 13:38:41 +0700

    added cat_names field to supplier serializer

--- main/emergency/serializers.py
+++ main/emergency/serializers.py
@@ -21,9 +21,15 @@ class HelpSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class SupplierSerializer(serializers.ModelSerializer):
+    cat_names = serializers.SerializerMethodField()
+
     class Meta:
         model = Suppliers
         fields = '__all__'
+    def get_cat_names(self, c):
+        category_name = [ category.name for category in c.cat.all()]
+        print(category_name)
+        return category_name
 
 class BlogpostSerializer(serializers.ModelSerializer):
     class Meta:
